efd97e1ae18ca47dca3501a173a7ec5b6ad70114
Bilalkhan1396 <biluu900999@gmail.com>
2024-06-29 01:57:44 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+// SHABANG  (uper file is not ordinary anf that code is executable on terminal mean can publish etc too)
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "enter your first number", type: "number", name: "firstnumber" },
+  { message: "enter your second number", type: "number", name: "secondnumber" },
+  {
+    message: "select one of the action to perfrom operation",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
+]);
+console.log(answer);
+
+// now usinng if,else condition=conditional statment
+if (answer.operator==="addition"){
+    console.log(answer.firstnumber+answer.secondnumber);
+}else if (answer.operator==="subtraction"){
+    console.log(answer.firstnumber-answer.secondnumber);
+}else if (answer.operator==="multiplication"){
+    console.log(answer.firstnumber*answer.secondnumber);
+}else if (answer.operator==="division"){
+    console.log(answer.firstnumber/answer.secondnumber);
+}else{
+    console.log("please select a valild operator")
+}
